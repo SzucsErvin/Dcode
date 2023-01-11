c229f70afb7dd251b2bdf098b9a3d38dac70cdb9
Ervin Szűcs <47424933+SzucsErvin@users.noreply.github.com>
2023-01-11 15:28:57 +0100

    Add files via upload

---
+++ Dcode/setting.py
@@ -0,0 +1,17 @@
+level_map = [
+'                            ',
+'                            ',
+'                            ',
+' XX    XXX            XX    ',
+' XX                         ',
+' XXXX         XX         XX ',
+' XXXX       XX    XX  XX    ',
+' XX    X  XXXX    XX  XXX   ',
+' XX    X  XXXX    XX  XXX   ',
+'    XXXX  XXXXXX  XX  XXXX  ',
+'XXXXXXXX  XXXXXX  XX  XXXX  ',]
+
+title_size = 64
+screen_width = 1200
+screen_height = len(level_map) * title_size
+print(screen_height)
